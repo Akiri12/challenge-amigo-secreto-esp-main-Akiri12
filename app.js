cf6441284af14112200ff34012c7bc76b5781bd4
Eduardo13-10 <eduardoalexisai@gmail.com>
2025-01-27 10:04:37 -0300

    Se añaden comentarios para facilitar lectura

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
-/* En este desafío, desarrollarás una aplicación que permita a los usuarios ingresar nombres de amigos en una lista para luego realizar un sorteo aleatorio y determinar quién es el "amigo secreto".
+/* Función para agregar amigos
 
-El usuario deberá agregar nombres mediante un campo de texto y un botón "Adicionar". Los nombres ingresados se mostrarán en una lista visible en la página, y al finalizar, un botón "Sortear Amigo" seleccionará uno de los nombres de forma aleatoria, mostrando el resultado en pantalla. Fucionalidades:
-
-- Agregar nombres: Los usuarios escribirán el nombre de un amigo en un campo de texto y lo agregarán a una lista visible al hacer clic en "Adicionar".
-- Validar entrada: Si el campo de texto está vacío, el programa mostrará una alerta pidiendo un nombre válido.
-- Visualizar la lista: Los nombres ingresados aparecerán en una lista debajo del campo de entrada.
-- Sorteo aleatorio: Al hacer clic en el botón "Sortear Amigo", se seleccionará aleatoriamente un nombre de la     lista y se mostrará en la página.
-
-Función para agregar amigos
-
-Desarrolla una función, que permita al usuario ingresar un nombre en el campo de texto y añadirlo a la lista de amigos creada anteriormente.
-Tareas específicas:
+Desarrolla una función, que permita al usuario ingresar un nombre en el campo de texto y añadirlo a la lista de amigos creada anteriormente. Tareas específicas:
 
 - Capturar el valor del campo de entrada: Utilizar document.getElementById o document.querySelector para obtener el texto ingresado por el usuario.
 - Validar la entrada: Implementar una validación para asegurarse de que el campo no esté vacío. Si está vacío, mostrar un alert con un mensaje de error: "Por favor, inserte un nombre."
@@ -24,7 +14,7 @@ function agregarAmigos() {
     // Capturar el nombre del campo de entrada: Utilizar document.getElementById para obtener el texto ingresado por el usuario.
     const nombreAmigo = document.getElementById('amigo').value;
 
-    // Verificar si el campo de entrada está vacío, trim() elimina los espacios en blanco al principio y al final de una cadena.
+    // Verificar si el campo de entrada está vacío. Trim() elimina los espacios en blanco al principio y al final de una cadena.
     if (!nombreAmigo.trim()) {
         // Validar la entrada: Implementar una validación para asegurarse de que el campo no esté vacío. Si está vacío, mostrar un alert con un mensaje de error: "Por favor, inserte un nombre."
         alert('Por favor, inserte un nombre.');
